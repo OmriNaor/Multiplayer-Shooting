55d2c381fba6e9d558e12981f7ed4c218ed335d4
Omri Naor <omrin2011@gmail.com>
2023-08-19 13:40:55 +0300

    Created project

---
+++ README.md
@@ -0,0 +1,3 @@
+# MultiplayerShooting
+
+Developed with Unreal Engine 5
---
+++ Source/MultiplayerShooting/MultiplayerShootingGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MultiplayerShootingGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MULTIPLAYERSHOOTING_API AMultiplayerShootingGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
